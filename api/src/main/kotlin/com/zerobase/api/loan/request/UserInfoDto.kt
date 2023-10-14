bebeb07c59c8fc1b7e7606a5ee7dec331ee679c3
soeun135 <soensuckun@naver.com>
2023-10-15 02:06:36 +0900

    API 개발, 암/복호화 구현

---
+++ api/src/main/kotlin/com/zerobase/api/loan/request/UserInfoDto.kt
@@ -0,0 +1,18 @@
+package com.zerobase.api.loan.request
+
+import com.zerobase.domain.domain.UserInfo
+
+class UserInfoDto(
+    val userKey: String,
+    val userName: String,
+    val userRegistrationNumber: String,
+    val userIncomeAmount: Long
+) {
+    fun toEntity(): UserInfo =
+        UserInfo(
+            userKey,
+            userRegistrationNumber,
+            userName,
+            userIncomeAmount
+        )
+}
