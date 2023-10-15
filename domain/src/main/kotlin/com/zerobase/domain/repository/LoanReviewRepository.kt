b4c87c0a8f51678bf654d2d9c06d55ab1c1998c9
soeun135 <soensuckun@naver.com>
2023-10-15 16:54:11 +0900

    예외처리 구현

--- domain/src/main/kotlin/com/zerobase/domain/repository/LoanReviewRepository.kt
+++ domain/src/main/kotlin/com/zerobase/domain/repository/LoanReviewRepository.kt
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository
 
 @Repository
 interface LoanReviewRepository : JpaRepository<LoanReview, Long> {
-    fun findByUserKey(userKey: String): LoanReview
+    fun findByUserKey(userKey: String): LoanReview?
 }
